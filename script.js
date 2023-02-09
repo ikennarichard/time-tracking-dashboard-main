f5b64221243a52c6efa33dd98916dec528fc72a0
Ikenna Richard <oguejioforrichard@gmail.com>
2023-02-08 19:25:41 -1100

    Add details for timeframes in script file

---
+++ script.js
@@ -0,0 +1,164 @@
+
+const data = [
+    {
+      "title": "Work",
+      "timeframes": {
+        "daily": {
+          "current": 5,
+          "previous": 7
+        },
+        "weekly": {
+          "current": 32,
+          "previous": 36
+        },
+        "monthly": {
+          "current": 103,
+          "previous": 128
+        }
+      }
+    },
+    {
+      "title": "Play",
+      "timeframes": {
+        "daily": {
+          "current": 1,
+          "previous": 2
+        },
+        "weekly": {
+          "current": 10,
+          "previous": 8
+        },
+        "monthly": {
+          "current": 23,
+          "previous": 29
+        }
+      }
+    },
+    {
+      "title": "Study",
+      "timeframes": {
+        "daily": {
+          "current": 0,
+          "previous": 1
+        },
+        "weekly": {
+          "current": 4,
+          "previous": 7
+        },
+        "monthly": {
+          "current": 13,
+          "previous": 19
+        }
+      }
+    },
+    {
+      "title": "Exercise",
+      "timeframes": {
+        "daily": {
+          "current": 1,
+          "previous": 1
+        },
+        "weekly": {
+          "current": 4,
+          "previous": 5
+        },
+        "monthly": {
+          "current": 11,
+          "previous": 18
+        }
+      }
+    },
+    {
+      "title": "Social",
+      "timeframes": {
+        "daily": {
+          "current": 1,
+          "previous": 3
+        },
+        "weekly": {
+          "current": 5,
+          "previous": 10
+        },
+        "monthly": {
+          "current": 21,
+          "previous": 23
+        }
+      }
+    },
+    {
+      "title": "Self Care",
+      "timeframes": {
+        "daily": {
+          "current": 0,
+          "previous": 1
+        },
+        "weekly": {
+          "current": 2,
+          "previous": 2
+        },
+        "monthly": {
+          "current": 7,
+          "previous": 11
+        }
+      }
+    }
+  ]
+
+
+const timeFrames = document.querySelectorAll('.time_of_report > small');
+
+const current = [...document.querySelectorAll('.bg .duration > p')];
+
+const previous = [...document.querySelectorAll('.bg .previous')];
+
+
+
+
+
+function setTimeFrames() {
+    try {
+    timeFrames.forEach((time) => time.addEventListener('click', (e) => {
+        select(e);
+        setValues(e)
+    }))
+    }
+
+
+catch(e) {
+    console.log(e)
+}
+}
+
+setTimeFrames()
+
+
+function setValues(e) {
+    for(let i in data) {
+        if (e.target.innerText == "Daily") {
+            current[i].innerText = data[i].timeframes.daily.current + 'hrs'
+            previous[i].innerText = data[i].timeframes.daily.previous + 'hrs'
+        }  else if (e.target.innerText == "Weekly") {
+          current[i].innerText = data[i].timeframes.weekly.current + 'hrs'
+          previous[i].innerText = data[i].timeframes.weekly.previous + 'hrs'   
+      }
+        
+        else if (e.target.innerText == "Monthly") {
+            current[i].innerText = data[i].timeframes.monthly.current + 'hrs'
+            previous[i].innerText = data[i].timeframes.monthly.previous + 'hrs'   
+        }
+    }
+}
+
+setValues()
+
+
+
+function select(e) {
+    timeFrames.forEach((time) => {
+        if (time == e.target) {
+            time.classList.add('selected');
+        } else {
+            time.classList.remove('selected');
+        }
+    })
+}
